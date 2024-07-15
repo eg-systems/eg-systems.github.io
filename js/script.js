c29cce00b5f5a519b30a719a25f1706e3c107149
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-07-15 14:48:56 +0300

    add - add emailjs to info@egsysco.com

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 (function(){
-    emailjs.init("LY09EPufKh4hH0f7k");
+    emailjs.init("rhLgr1qJ_xsHX45E3");
 })();
 
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    const serviceID = 'service_141uf43';
-    const templateID = 'template_7793lfq';
+    const serviceID = 'service_mnjshrw';
+    const templateID = 'template_8i8mvqd';
 
     emailjs.sendForm(serviceID, templateID, this)
         .then(function(response) {
